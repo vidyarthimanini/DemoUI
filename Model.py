b26b97c167e3a57c3cf80118911e30439c51083c
vidyarthimanini <140050683+vidyarthimanini@users.noreply.github.com>
2025-12-04 12:52:42 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.linear_model import Ridge
 import joblib
-from google.colab import files
+
 
 warnings.filterwarnings("ignore")
 
